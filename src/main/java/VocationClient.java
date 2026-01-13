743457787595d32f4530a2b21fdde4d9d65a2c0f
umidovalimardon06 <umidovalimardon06@gmail.com>
2026-01-13 09:48:44 +0500

    facade implementation

--- src/main/java/VocationClient.java
+++ src/main/java/VocationClient.java
@@ -5,15 +5,8 @@ public class VocationClient {
         LocalDate startDate = LocalDate.of(2026,1,15);
         LocalDate endDate = LocalDate.of(2026,5,15);
 
-        FlightRental flightRental = new FlightRental("F23");
-        flightRental.bookOutwardJourney(startDate);
-        flightRental.bookOutwardJourney(endDate);
-
-        HotelRental hotelRental = new HotelRental("MiY");
-        hotelRental.book(startDate,endDate);
-
-        CarRental carRental = new CarRental("Mazda");
-        carRental.book(startDate,endDate);
+        VacationFacade facade = new VacationFacade();
+        facade.book(startDate,endDate);
 
     }
 }
